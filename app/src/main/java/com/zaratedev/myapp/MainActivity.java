80d43f98798cd0f3798efde9eec9e954ec6450ea
zaratedev <zaratedev@gmail.com>
2017-12-27 23:39:28 -0600

    Scroll TextView - web service

--- app/src/main/java/com/zaratedev/myapp/MainActivity.java
+++ app/src/main/java/com/zaratedev/myapp/MainActivity.java
@@ -1,74 +1,38 @@
 package com.zaratedev.myapp;
 
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView texto;
     Button boton;
+    TextView texto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        texto = findViewById(R.id.texto);
         boton = findViewById(R.id.boton);
+        texto = findViewById(R.id.texto);
+
+        texto.setMovementMethod(new ScrollingMovementMethod());
 
         boton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                new MyAsycTask().execute(100);
+                for (int i = 0; i <= 100; i++) {
+                    cargarDatos("Número: "+ i);
+                }
             }
         });
     }
 
-
-    public class MyAsycTask extends AsyncTask<Integer, Integer, String>{
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-        }
-
-        @Override
-        protected String doInBackground(Integer... integers) {
-            int max = integers[0];
-
-            for (int i = 1; i < max; i++) {
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-                publishProgress(i);
-            }
-            return "End";
-        }
-
-        @Override
-        protected void onProgressUpdate(Integer... values) {
-            super.onProgressUpdate(values);
-
-            int contador = values[0];
-            String text = "Contador" + contador;
-
-            texto.setText(text);
-            texto.setTextSize(contador);
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-
-            texto.append("\n" + s);
-        }
+    public void cargarDatos(String datos) {
+        texto.append(datos + "\n" );
     }
 }
